a2f27c2758111b3cdc08ec0fd8a001b3b8cb1a03
Robert Medeiros <robert@crimeminister.org>
2015-06-14 23:32:19 -0400

    bump schema and transit-clj dependency versions

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ca.clojurist/revuecinema "0.1.0-SNAPSHOT"]
                  [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
-                 [com.cognitect/transit-clj "0.8.271"]
+                 [com.cognitect/transit-clj "0.8.275"]
                  [com.stuartsierra/component "0.2.3"]
                  [io.pedestal/pedestal.jetty "0.4.0"]
                  [io.pedestal/pedestal.service "0.4.0"]
@@ -17,7 +17,7 @@
                  [org.slf4j/jcl-over-slf4j "1.7.12" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.12" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/log4j-over-slf4j "1.7.12" :exclusions [org.slf4j/slf4j-api]]
-                 [prismatic/schema "0.4.2"]]
+                 [prismatic/schema "0.4.3"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "revuecinema.server/run-dev"]}
