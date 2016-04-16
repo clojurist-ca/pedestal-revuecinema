9181e12b50b7affabef874287754ec78485bd6f4
Robert Medeiros <robert@crimeminister.org>
2016-04-15 23:46:52 -0400

    Add scripts for building/running/signing ACI

---
+++ appc-run.sh
@@ -0,0 +1,1 @@
+rkt run --net=default --port=http:8000 --dns=8.8.8.8 api-revuecinema-0.0.1-SNAPSHOT-linux-amd64.aci
---
+++ appc-sign.sh
@@ -0,0 +1,1 @@
+gpg --armor --detach-sign api-revuecinema-0.0.1-SNAPSHOT-linux-amd64.aci
