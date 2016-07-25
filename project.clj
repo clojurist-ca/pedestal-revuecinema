091d56f27b82bb44623ee0dd5aa6ef2e5d103b35
Robert Medeiros <robert.medeiros@cbc.ca>
2016-07-25 14:34:55 -0400

    WIP

--- project.clj
+++ project.clj
@@ -3,24 +3,26 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ca.clojurist/revuecinema "0.1.0-SNAPSHOT"]
-                 [ch.qos.logback/logback-classic "1.1.5" :exclusions [org.slf4j/slf4j-api]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [com.cognitect/transit-clj "0.8.285"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [io.pedestal/pedestal.jetty "0.4.1"]
-                 [io.pedestal/pedestal.service "0.4.1"]
-                 [org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/core.async "0.2.374"]
-                 [org.clojure/core.typed "0.3.22"]
+                 [io.pedestal/pedestal.jetty "0.5.0"]
+                 [io.pedestal/pedestal.service "0.5.0"]
+                 [org.clojure/core.async "0.2.385"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/test.check "0.9.0"]
-                 [org.slf4j/jcl-over-slf4j "1.7.18" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.18" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/log4j-over-slf4j "1.7.18" :exclusions [org.slf4j/slf4j-api]]
-                 [prismatic/schema "1.0.5"]]
+                 [org.clojure/tools.reader "1.0.0-beta3"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jcl-over-slf4j "1.7.21" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.21" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/log4j-over-slf4j "1.7.21" :exclusions [org.slf4j/slf4j-api]]
+                 [enlive "1.1.6"]
+                 [prismatic/schema "1.1.3"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
+  :plugins [[info.sunng/lein-bootclasspath-deps "0.2.0"]]
+  :boot-dependencies [[org.mortbay.jetty.alpn/alpn-boot "8.1.4.v20150727"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "revuecinema.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.4.1"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.0"]]}
              :uberjar {:aot [revuecinema.server]}}
   :main ^{:skip-aot true} revuecinema.server)
